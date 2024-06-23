412b1231b449b5d3b24f16ed76d75f58be0dbc89
Andrés Guerrero <andresguerrero016@gmail.com>
2024-06-23 16:20:41 -0300

    agrega imágenes etc

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { Book } from './book';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrl: './book-list.component.scss'
+})
+export class BookListComponent {
+
+
+  books: Book []= [
+    {
+      title: "Biología",
+      author: "Curtis",
+      price: 24000,
+      stock: 21,
+      image: "assets/img/biologia-Curtis.png",
+      clearance: false,
+    },
+    {
+      title: "Crónicas marcianas",
+      author: "Ray Bradbury",
+      price: 14000,
+      stock: 7,
+      image: "assets/img/cronicas-marcianas.jpg",
+      clearance: false,
+    },
+    {
+      title: "Dune",
+      author: "Frank Herbert",
+      price: 20000,
+      stock: 12,
+      image: "assets/img/dune.jpg",
+      clearance: true,
+    },
+    {
+      title: "Historias de la Prehistoria",
+      author: "David Benito del Olmo",
+      price: 10000,
+      stock: 5,
+      image: "assets/img/historias-de-la-prehistoria.jpg",
+      clearance: true,
+    },
+    {
+      title: "Mujeres tenían que ser",
+      author: "Felipe Pigna",
+      price: 13000,
+      stock: 11,
+      image: "assets/img/mujeres-tenian-que-ser.jpg",
+      clearance: false,
+    },
+    {
+      title: "The Biology Book",
+      author: "DK",
+      price: 34000,
+      stock: 0,
+      image: "assets/img/the-biology-book.jpg",
+      clearance: false,
+    }
+  ]
+}
